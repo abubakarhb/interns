7efcda75a48fdcb913620d3789642f9598c8e9c8
mustapha jay <mustaphajay2@gmail.com>
2022-06-22 23:10:19 +0100

    added the chat area and modified styles also included remaining icons

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -5,40 +5,56 @@ import { TbPencil } from "react-icons/tb";
 import { FaRegStickyNote } from "react-icons/fa";
 import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
 import { FiMessageSquare } from "react-icons/fi";
+import { GiArrowCursor } from "react-icons/gi";
+import { BsArrow90DegRight, BsArrow90DegLeft } from "react-icons/bs";
 
 const Chat = () => {
   return (
-    <div className="flex flex-start gap-[3rem] h-[100vh]">
-      <div className="flex flex-col justify-evenly ">
-        <span className="text-[1.3rem]">
-          {" "}
-          <TiLocationArrowOutline />
-        </span>
-        <span className="text-[1.3rem]">
-          {" "}
-          <RiText />
-        </span>
+    <div className="flex flex-start gap-4 h-[100vh] pt-[5rem] bg-[#f5f5f5]">
+      <div className="grid  gap-4 mt-[4rem] px-4 ">
+        <div className="flex gap-[2rem] flex-col items-center">
+          <span className="text-base bg-white rounded-[50%] p-2">
+            {" "}
+            <TiLocationArrowOutline />
+          </span>
+          <span className="text-base bg-white rounded-[50%] p-2">
+            {" "}
+            <RiText />
+          </span>
 
-        <span className="text-[1.3rem]">
-          <TbPencil />
-        </span>
-        <span className="text-[1.3rem]">
-          {" "}
-          <FaRegStickyNote />
-        </span>
+          <span className="text-base bg-white rounded-[50%] p-2">
+            <TbPencil />
+          </span>
+          <span className="text-base bg-white rounded-[50%] p-2">
+            {" "}
+            <FaRegStickyNote />
+          </span>
 
-        <span className="text-[1.3rem]">
-          <MdOutlineCheckBoxOutlineBlank />
-        </span>
-        <span className="text-[1.3rem]">
-          {" "}
-          <FiMessageSquare />
-        </span>
+          <span className="text-base bg-white rounded-[50%] p-2">
+            <MdOutlineCheckBoxOutlineBlank />
+          </span>
+          <span className="text-base bg-white rounded-[50%] p-2">
+            {" "}
+            <FiMessageSquare />
+          </span>
+        </div>
+        <div className="flex gap-2 items-center ">
+          <span className="text-base bg-white rounded-[50%] p-2">
+            <BsArrow90DegLeft />
+          </span>
+          <span className="text-base bg-white rounded-[50%] p-2">
+            <BsArrow90DegRight />
+          </span>
+        </div>
       </div>
-      <section className='mt-[5rem]'>
-        <p>we neeed to</p>
-
-        <p>maria</p>
+      <section className="w-[70%] bg-white pt-[5rem] px-[5rem] rounded-sm">
+        <p className="px-2 bg-purple-100 rounded-sm w-fit">we need to</p>
+        <div className="flex ml-[6rem]">
+          <span className="text-lg">
+            <GiArrowCursor />
+          </span>
+          <p className=" bg-purple-400 px-2 rounded-sm w-fit">maria</p>
+        </div>
       </section>
     </div>
   );
