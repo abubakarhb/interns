8e293635140781fbdeab1fa6927dadb1c68a43ed
mustapha jay <mustaphajay2@gmail.com>
2022-06-22 15:27:29 +0100

    added charoom, chat and sidebar components

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-import Chatroom from './Chatroom'
+import Chatroom from "./Chatroom";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
 const Content = () => {
-  return  <div > 
-<Chatroom/>
-  </div>;
+  return (
+    <div className="flex justify-between">
+      <Sidebar />
+      <div className="mt-[5rem]">
+        <Chat />
+      </div>
+      <Chatroom />
+    </div>
+  );
 };
 
 export default Content;
