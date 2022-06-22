b3a044e07bd09bb3fe079333f2d1981d8bea5046
mustapha jay <mustaphajay2@gmail.com>
2022-06-23 00:42:24 +0100

    added margin-top to icons

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { sidebarIcons, sidebarIconsTwo } from "../iconData";
 const Sidebar = () => {
   return (
     <div className="flex flex-col justify-between flex-start gap-[2rem] items-center mb-3 ">
-      <div className="flex gap-[2rem] flex-col items-center">
+      <div className="flex gap-[2rem] mt-[1.5rem] flex-col items-center">
         {sidebarIcons.map((icons) => {
           return (
             <span key={icons.id} className="text-lg">
