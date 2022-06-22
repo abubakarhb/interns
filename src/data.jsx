e262c5f05b41a7cb2fa2092eac0c65d772d7a1a5
mustapha jay <mustaphajay2@gmail.com>
2022-06-22 12:56:06 +0100

    added chatroom data content

---
+++ src/data.jsx
@@ -0,0 +1,38 @@
+export const people = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "John Doe",
+    text: "lorem lorem lorem",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "John Doe",
+    text: "lorem lorem lorem",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "John Doe",
+    text: "lorem lorem lorem",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "John Doe",
+    text: "lorem lorem lorem",
+  },
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "John Doe",
+    text: "lorem lorem lorem",
+  },
+  {
+    id: 6,
+    img: "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "John Doe",
+    text: "lorem lorem lorem",
+  },
+];
