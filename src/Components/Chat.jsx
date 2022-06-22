8af4425c97d9018fe73d4cf4ad8bed602a1a96e8
mustapha jay <mustaphajay2@gmail.com>
2022-06-23 00:27:04 +0100

    refactored and moved icons to a different file

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,50 +1,34 @@
 import React from "react";
-import { TiLocationArrowOutline } from "react-icons/ti";
-import { RiText } from "react-icons/ri";
-import { TbPencil } from "react-icons/tb";
-import { FaRegStickyNote } from "react-icons/fa";
-import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
-import { FiMessageSquare } from "react-icons/fi";
+import { chatIcons, chatIconsTwo } from "../iconData";
 import { GiArrowCursor } from "react-icons/gi";
-import { BsArrow90DegRight, BsArrow90DegLeft } from "react-icons/bs";
 
 const Chat = () => {
   return (
     <div className="flex flex-start gap-4 h-[100vh] pt-[5rem] bg-[#f5f5f5]">
       <div className="grid  gap-4 mt-[4rem] px-4 ">
         <div className="flex gap-[2rem] flex-col items-center">
-          <span className="text-base bg-white rounded-[50%] p-2">
-            {" "}
-            <TiLocationArrowOutline />
-          </span>
-          <span className="text-base bg-white rounded-[50%] p-2">
-            {" "}
-            <RiText />
-          </span>
-
-          <span className="text-base bg-white rounded-[50%] p-2">
-            <TbPencil />
-          </span>
-          <span className="text-base bg-white rounded-[50%] p-2">
-            {" "}
-            <FaRegStickyNote />
-          </span>
-
-          <span className="text-base bg-white rounded-[50%] p-2">
-            <MdOutlineCheckBoxOutlineBlank />
-          </span>
-          <span className="text-base bg-white rounded-[50%] p-2">
-            {" "}
-            <FiMessageSquare />
-          </span>
+          {chatIcons.map((icons) => {
+            return (
+              <span
+                key={icons.id}
+                className="text-base bg-white rounded-[50%] p-2"
+              >
+                {icons.icon}
+              </span>
+            );
+          })}
         </div>
         <div className="flex gap-2 items-center ">
-          <span className="text-base bg-white rounded-[50%] p-2">
-            <BsArrow90DegLeft />
-          </span>
-          <span className="text-base bg-white rounded-[50%] p-2">
-            <BsArrow90DegRight />
-          </span>
+          {chatIconsTwo.map((icons) => {
+            return (
+              <span
+                key={icons.key}
+                className="text-base bg-white rounded-[50%] p-2"
+              >
+                {icons.icon}
+              </span>
+            );
+          })}
         </div>
       </div>
       <section className="w-[70%] bg-white pt-[5rem] px-[5rem] rounded-sm">
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,47 +1,25 @@
-import { TiLocationArrowOutline } from "react-icons/ti";
-import { RiText } from "react-icons/ri";
-import { TbPencil } from "react-icons/tb";
-import { FaRegStickyNote } from "react-icons/fa";
-import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
-import { FiMessageSquare } from "react-icons/fi";
-import { BsPerson, BsBell } from "react-icons/bs";
+import { sidebarIcons, sidebarIconsTwo } from "../iconData";
 
 const Sidebar = () => {
   return (
     <div className="flex flex-col justify-between flex-start gap-[2rem] items-center mb-3 ">
       <div className="flex gap-[2rem] flex-col items-center">
-        <span className="text-[1.3rem]">
-          {" "}
-          <TiLocationArrowOutline />
-        </span>
-        <span className="text-[1.3rem]">
-          {" "}
-          <RiText />
-        </span>
-
-        <span className="text-[1.3rem]">
-          <TbPencil />
-        </span>
-        <span className="text-[1.3rem]">
-          {" "}
-          <FaRegStickyNote />
-        </span>
-
-        <span className="text-[1.3rem]">
-          <MdOutlineCheckBoxOutlineBlank />
-        </span>
-        <span className="text-[1.3rem]">
-          {" "}
-          <FiMessageSquare />
-        </span>
+        {sidebarIcons.map((icons) => {
+          return (
+            <span key={icons.id} className="text-lg">
+              {icons.icon}
+            </span>
+          );
+        })}
       </div>
       <div className="flex flex-col gap-[2rem]">
-        <span className="text-[1.3rem]">
-          <BsPerson />
-        </span>
-        <span className="text-[1.3rem]">
-          <BsBell />
-        </span>
+        {sidebarIconsTwo.map((icons) => {
+          return (
+            <span key={icons.id} className="text-lg">
+              {icons.icon}
+            </span>
+          );
+        })}
       </div>
     </div>
   );
