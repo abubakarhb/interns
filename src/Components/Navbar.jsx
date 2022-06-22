6cfc850150a8454a29f0eadf65bb86a68f682717
mustapha jay <mustaphajay2@gmail.com>
2022-06-22 15:00:53 +0100

    added styling to navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,25 +6,51 @@ import { HiOutlineMicrophone } from "react-icons/hi";
 import { FaTimes } from "react-icons/fa";
 const Navbar = () => {
   return (
-    <div className="flex justify-around p-4">
-      <h1 className="text-lg uppercase flex items-center px-1">
+    <div className="flex justify-around p-4 items-center bg-[#f5f5f5]">
+      <h1 className="text-lg uppercase flex font-semibold items-center px-1">
         c2001: intro to programming{" "}
-        <span>
+        <span className="ml-2">
           <RiErrorWarningLine />
         </span>
       </h1>
-      <div className="flex justify-evenly">
-        <GrDocumentNotes />
+      <div className="flex  items-center justify-evenly">
+        <div className="mx-2 bg-yellow-200 p-2 rounded-[50%]">
+          <GrDocumentNotes />
+        </div>
 
-        <BsStopCircle />
-        <HiOutlineMicrophone />
-        <BsCameraVideo />
-        <FaTimes />
+        <div className="mx-2 bg-purple-200 p-2 rounded-[50%]">
+          {" "}
+          <BsStopCircle />
+        </div>
+        <div className="mx-2 bg-blue-200 p-2 rounded-[50%]">
+          {" "}
+          <HiOutlineMicrophone />
+        </div>
+        <div className="mx-2 bg-gray-200 p-2 rounded-[50%]">
+          {" "}
+          <BsCameraVideo />
+        </div>
+        <div className="mx-2 bg-red-200 p-2 rounded-[50%]">
+          {" "}
+          <FaTimes />
+        </div>
       </div>
       <div className="flex">
-        <BsFillPersonFill />
-        <BsFillPersonFill />
-        <BsFillPersonFill />
+        <img
+          className="w-[30px] h-[30px] rounded-[50%]"
+          src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <img
+          className="w-[30px] h-[30px] rounded-[50%]"
+          src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <img
+          className="w-[30px] h-[30px] rounded-[50%]"
+          src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
       </div>
     </div>
   );
