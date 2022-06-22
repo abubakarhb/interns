e715dbb0ef99346c899042b86dc87f8f47c05f09
mustapha jay <mustaphajay2@gmail.com>
2022-06-22 12:09:55 +0100

    added navbar icons

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { RiErrorWarningLine } from "react-icons/ri";
+import { GrDocumentNotes } from "react-icons/gr";
+import { BsStopCircle, BsCameraVideo, BsFillPersonFill } from "react-icons/bs";
+import { HiOutlineMicrophone } from "react-icons/hi";
+import { FaTimes } from "react-icons/fa";
 const Navbar = () => {
   return (
-    <div>
-      <h1 className="text-lg uppercase">
+    <div className="flex justify-around p-4">
+      <h1 className="text-lg uppercase flex items-center px-1">
         c2001: intro to programming{" "}
         <span>
           <RiErrorWarningLine />
         </span>
       </h1>
       <div className="flex justify-evenly">
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
+        <GrDocumentNotes />
+
+        <BsStopCircle />
+        <HiOutlineMicrophone />
+        <BsCameraVideo />
+        <FaTimes />
       </div>
       <div className="flex">
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
+        <BsFillPersonFill />
+        <BsFillPersonFill />
+        <BsFillPersonFill />
       </div>
     </div>
   );
