05c8454d3ffb29967d056e70390de66c0ca0ed14
mustapha jay <mustaphajay2@gmail.com>
2022-06-23 00:21:00 +0100

    initial commit

---
+++ src/iconData.jsx
@@ -0,0 +1,67 @@
+import { TiLocationArrowOutline } from "react-icons/ti";
+import { RiText, RiPulseFill } from "react-icons/ri";
+import { TbPencil } from "react-icons/tb";
+import { FaRegStickyNote } from "react-icons/fa";
+import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
+import { FiMessageSquare, FiVideo } from "react-icons/fi";
+import { BsArrow90DegRight, BsArrow90DegLeft } from "react-icons/bs";
+import { BiNotepad } from "react-icons/bi";
+import { AiOutlineCalendar } from "react-icons/ai";
+
+
+export const chatIcons = [
+  {
+    id: 1,
+    icon: <TiLocationArrowOutline />,
+  },
+  {
+    id: 2,
+    icon: <RiText />,
+  },
+  {
+    id: 3,
+    icon: <TbPencil />,
+  },
+  {
+    id: 4,
+    icon: <FaRegStickyNote />,
+  },
+  {
+    id: 5,
+    icon: <MdOutlineCheckBoxOutlineBlank />,
+  },
+  {
+    id: 6,
+    icon: <FiMessageSquare />,
+  },
+];
+
+export const chatIconsTwo = [
+  {
+    id: 7,
+    icon: <BsArrow90DegLeft />,
+  },
+  {
+    id: 8,
+    icon: <BsArrow90DegRight />,
+  },
+];
+
+export const sidebarIcons = [
+  {
+    id: 1,
+    icon: <BiNotepad />,
+  },
+  {
+    id: 2,
+    icon: <AiOutlineCalendar />,
+  },
+  {
+    id: 3,
+    icon: <FiVideo />,
+  },
+  {
+    id: 4,
+    icon: <RiPulseFill />,
+  },
+];
