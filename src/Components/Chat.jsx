07284289b833989d3faed879cc1a4f5875d4d4da
mustapha jay <mustaphajay2@gmail.com>
2022-06-23 10:24:57 +0100

    added keypoint cards to section

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -31,7 +31,7 @@ const Chat = () => {
           })}
         </div>
       </div>
-      <section className="w-[70%] bg-white pt-[5rem] px-[5rem] rounded-sm">
+      <section className="w-[70%] bg-white pt-[5rem] px-[5rem] rounded-sm relative">
         <p className="px-2 bg-purple-100 rounded-sm w-fit">we need to</p>
         <div className="flex ml-[6rem]">
           <span className="text-lg">
@@ -39,6 +39,29 @@ const Chat = () => {
           </span>
           <p className=" bg-purple-400 px-2 rounded-sm w-fit">maria</p>
         </div>
+        <article className="absolute bottom-[4rem] right-[-4rem]">
+          <div className="bg-blue-200 p-4 px-6 mb-3 ">
+            <h1 className="mb-4">Key Points</h1>
+            <p>1. pick an industry</p>
+            <p>2. find a trending problem</p>
+            <div className="flex ml-[6rem]">
+              <span className="text-lg">
+                <GiArrowCursor />
+              </span>
+              <p className=" bg-red-300/50 px-2 rounded-sm mt-2">Geroge</p>
+            </div>
+          </div>
+          <div className="bg-yellow-200 p-4 px-6 ">
+            <h1 className="mb-4">Key Points</h1>
+            <p>1. its unclear</p>
+            <div className="flex ml-[6rem]">
+              <span className="text-lg">
+                <GiArrowCursor />
+              </span>
+              <p className=" bg-green-400/60 px-2 rounded-sm mt-2">Geroge</p>
+            </div>
+          </div>
+        </article>
       </section>
     </div>
   );
