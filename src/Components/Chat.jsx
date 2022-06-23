129f1430f00b0b04d08a719b14f180dffdf2a0fe
mustapha jay <mustaphajay2@gmail.com>
2022-06-23 11:21:42 +0100

    refactored card article section

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Keypoints from "./Keypoints";
 import { chatIcons, chatIconsTwo } from "../iconData";
 import { GiArrowCursor } from "react-icons/gi";
 
@@ -7,6 +7,7 @@ const Chat = () => {
     <div className="flex flex-start gap-4 h-[100vh] pt-[5rem] bg-[#f5f5f5]">
       <div className="grid  gap-4 mt-[4rem] px-4 ">
         <div className="flex gap-[2rem] flex-col items-center">
+          {/* chaticons  */}
           {chatIcons.map((icons) => {
             return (
               <span
@@ -39,29 +40,8 @@ const Chat = () => {
           </span>
           <p className=" bg-purple-400 px-2 rounded-sm w-fit">maria</p>
         </div>
-        <article className="absolute bottom-[4rem] right-[-4rem]">
-          <div className="bg-blue-200 p-4 px-6 mb-3 ">
-            <h1 className="mb-4">Key Points</h1>
-            <p>1. pick an industry</p>
-            <p>2. find a trending problem</p>
-            <div className="flex ml-[6rem]">
-              <span className="text-lg">
-                <GiArrowCursor />
-              </span>
-              <p className=" bg-red-300/50 px-2 rounded-sm mt-2">Geroge</p>
-            </div>
-          </div>
-          <div className="bg-yellow-200 p-4 px-6 ">
-            <h1 className="mb-4">Key Points</h1>
-            <p>1. its unclear</p>
-            <div className="flex ml-[6rem]">
-              <span className="text-lg">
-                <GiArrowCursor />
-              </span>
-              <p className=" bg-green-400/60 px-2 rounded-sm mt-2">Geroge</p>
-            </div>
-          </div>
-        </article>
+        {/* card component */}
+        <Keypoints />
       </section>
     </div>
   );
